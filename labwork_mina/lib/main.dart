ad09616d0217a28cd754b7e18197d81797cc8905
mjrMina <minamarcjansen@gmail.com>
2023-03-27 15:53:55 +0800

    2ndcommit

--- labwork_mina/lib/main.dart
+++ labwork_mina/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:labwork_mina/screens/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
--- labwork_mina/lib/screens/home.dart
+++ labwork_mina/lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,8 +9,52 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var itemholder = TextEditingController();
+  List<String> itemshop = [];
+
+  void add() async {
+    await FirebaseFirestore.instance
+        .collection("minaMJR")
+        .add({"items": itemholder.text});
+    itemholder.clear();
+  }
+
+  void get() async {
+    Stream collectionStream =
+        FirebaseFirestore.instance.collection('items').snapshots();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lab Work"),
+      ),
+      body: Container(
+        child: SafeArea(
+            child: Column(
+          children: [
+            Card(
+              elevation: 10,
+              child: Column(children: [
+                Padding(
+                    padding: EdgeInsets.all(10),
+                    child: TextFormField(
+                      controller: itemholder,
+                      decoration: InputDecoration(border: OutlineInputBorder()),
+                    )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(onPressed: add, child: Text("Add Item"))
+                  ],
+                ),
+              ]),
+            ),
+            Expanded(child: StreamBuilder<QuerySnapshot>())
+          ],
+        )),
+      ),
+    );
   }
 }
